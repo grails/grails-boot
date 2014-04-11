2503a9dcf6f549f618a0492749acffde4b9a6269
Lari Hotari <lhotari@gopivotal.com>
2014-04-11 17:36:32 +0300

    use groovy 2.3.0-beta-2

--- sample-apps/gsp/script/app.groovy
+++ sample-apps/gsp/script/app.groovy
@@ -1,7 +1,7 @@
 //@GrabResolver(name='grailsSnapshots', root='http://repo.grails.org/grails/libs-snapshots-local')
-@Grab("org.codehaus.groovy:groovy-templates:2.3.0-beta-1")
-@Grab("org.codehaus.groovy:groovy-xml:2.3.0-beta-1")
-@Grab("org.codehaus.groovy:groovy:2.3.0-beta-1")
+@Grab("org.codehaus.groovy:groovy-templates:2.3.0-beta-2")
+@Grab("org.codehaus.groovy:groovy-xml:2.3.0-beta-2")
+@Grab("org.codehaus.groovy:groovy:2.3.0-beta-2")
 @Grab("org.grails:grails-gsp-spring-boot:1.0.0.BUILD-SNAPSHOT")
 import grails.gsp.TagLib
 import org.springframework.web.servlet.ModelAndView

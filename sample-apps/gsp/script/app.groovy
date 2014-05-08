66163d79de27d43f7432169e13cb0255225a8dcf
Lari Hotari <lhotari@gopivotal.com>
2014-05-08 16:51:58 +0300

    bump grails-gsp-spring-boot version to 1.0.0.RC1

--- sample-apps/gsp/script/app.groovy
+++ sample-apps/gsp/script/app.groovy
@@ -2,7 +2,7 @@
 @Grab("org.codehaus.groovy:groovy-templates:2.3.0")
 @Grab("org.codehaus.groovy:groovy-xml:2.3.0")
 @Grab("org.codehaus.groovy:groovy:2.3.0")
-@Grab("org.grails:grails-gsp-spring-boot:1.0.0.M2")
+@Grab("org.grails:grails-gsp-spring-boot:1.0.0.RC1")
 import grails.gsp.TagLib
 import org.springframework.web.servlet.ModelAndView
 import java.text.SimpleDateFormat

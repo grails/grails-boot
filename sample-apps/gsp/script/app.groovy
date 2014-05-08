43399dfb3f066f3a4d7dc7d3d8b9fe9d734405d9
Lari Hotari <lhotari@gopivotal.com>
2014-05-08 12:28:57 +0300

    use Grails 2.4.0.RC1

--- sample-apps/gsp/script/app.groovy
+++ sample-apps/gsp/script/app.groovy
@@ -1,8 +1,8 @@
 //@GrabResolver(name='grailsSnapshots', root='http://repo.grails.org/grails/libs-snapshots-local')
-@Grab("org.codehaus.groovy:groovy-templates:2.3.0-rc-1")
-@Grab("org.codehaus.groovy:groovy-xml:2.3.0-rc-1")
-@Grab("org.codehaus.groovy:groovy:2.3.0-rc-1")
-@Grab("org.grails:grails-gsp-spring-boot:1.0.0.M1")
+@Grab("org.codehaus.groovy:groovy-templates:2.3.0")
+@Grab("org.codehaus.groovy:groovy-xml:2.3.0")
+@Grab("org.codehaus.groovy:groovy:2.3.0")
+@Grab("org.grails:grails-gsp-spring-boot:1.0.0.M2")
 import grails.gsp.TagLib
 import org.springframework.web.servlet.ModelAndView
 import java.text.SimpleDateFormat

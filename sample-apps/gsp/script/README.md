8c6b2b75f2afa0f2ff807eaa364bb9bd1136a88f
Scott Murphy <scott@alwaysvip.com>
2023-07-11 14:23:04 -0700

    Update dependencies

--- sample-apps/gsp/script/README.md
+++ sample-apps/gsp/script/README.md
@@ -1,1 +1,6 @@
-Note: Use the 'run_app.sh' script as the example won't work with 'spring run app.groovy' until Boot updates to Groovy 2.3
+Use the following command with Spring Boot CLI 1.x
+
+spring run app.groovy
+
+1.x appears to not be downloadable using sdkman, but you can still download a snapshot from here: https://repo.spring.io/ui/native/snapshot/org/springframework/boot/spring-boot-cli/
+
--- sample-apps/gsp/script/app.groovy
+++ sample-apps/gsp/script/app.groovy
@@ -1,7 +1,7 @@
 //@GrabResolver(name='grailsSnapshots', root='http://repo.grails.org/grails/libs-snapshots-local')
-@Grab("org.codehaus.groovy:groovy-templates:2.3.7")
-@Grab("org.codehaus.groovy:groovy-xml:2.3.7")
-@Grab("org.codehaus.groovy:groovy:2.3.7")
+@Grab("org.codehaus.groovy:groovy-templates:3.0.18")
+@Grab("org.codehaus.groovy:groovy-xml:3.0.18")
+@Grab("org.codehaus.groovy:groovy:3.0.18")
 @Grab("org.grails:grails-gsp-spring-boot:1.0.0")
 import grails.gsp.TagLib
 import org.springframework.web.servlet.ModelAndView
